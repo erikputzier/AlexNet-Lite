7e72e1a3a312f8dfe2fb869a8ed846c81ab5907a
MaxAlexStumpf <maximilian.stumpf@mail.de>
2024-11-14 16:47:04 -0800

    Changes

--- Code/alexnet_model.py
+++ Code/alexnet_model.py
@@ -6,17 +6,22 @@ class AlexNet(tf.keras.Model):
         super(AlexNet, self).__init__()
 
         self.conv1 = layers.Conv2D(96, kernel_size = 11, strides = 4, padding = "same", activation = "relu")
+        self.bn1 = layers.BatchNormalization()
         self.pool1 = layers.MaxPooling2D(pool_size = 3, strides = 2)
 
-        self.conv2 = layers.Conv2D(256, kernel_size = 5, strides = 1, padding = "same", acitvation = "relu")
+        self.conv2 = layers.Conv2D(256, kernel_size = 5, strides = 1, padding = "same", activation = "relu")
+        self.bn2 = layers.BatchNormalization()
         self.pool2 = layers.MaxPooling2D(pool_size = 3, strides = 2)
 
         self.conv3 = layers.Conv2D(384, kernel_size = 3, strides = 1, padding = "same", activation = "relu")
+        self.bn3 = layers.BatchNormalization()
         self.conv4 = layers.Conv2D(384, kernel_size = 3, strides = 1, padding = "same", activation = "relu")
+        self.bn4 = layers.BatchNormalization()
         self.conv5 = layers.Conv2D(256, kernel_size = 3, strides = 1, padding = "same", activation = "relu")
-        self.pool5 = layers.MaxPooling2D(pool_size = 3, stride = 2)
+        self.bn5 = layers.BatchNormalization()
+        self.pool5 = layers.MaxPooling2D(pool_size = 3, strides = 2)
 
-        self.flatten = layers.Flatten()
+        self.global_pool = layers.GlobalAveragePooling2D()
         self.fc1 = layers.Dense(4096, activation = "relu")
         self.dropout1 = layers.Dropout(0.5)
         self.fc2 = layers.Dense(4096, activation = "relu")
@@ -25,17 +30,22 @@ class AlexNet(tf.keras.Model):
     
     def call(self, x):
         x = self.conv1(x)
+        x = self.bn1(x)
         x = self.pool1(x)
 
         x = self.conv2(x)
+        x = self.bn2(x)
         x = self.pool2(x)
 
         x = self.conv3(x)
+        x = self.bn3(x)
         x = self.conv4(x)
+        x = self.bn4(x)
         x = self.conv5(x)
+        x = self.bn5(x)
         x = self.pool5(x)
 
-        x = self.flatten(x)
+        x = self.global_pool(x)
         x = self.fc1(x)
         x = self.dropout1(x)
         x = self.fc2(x)
