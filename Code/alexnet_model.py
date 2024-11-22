832e13584cb9e9d7f51b4fda514dea52d224dee1
MaxAlexStumpf <maximilian.stumpf@mail.de>
2024-11-22 11:24:49 -0800

    gg

--- Code/alexnet_model.py
+++ Code/alexnet_model.py
@@ -1,40 +1,47 @@
 import tensorflow as tf
-from tensorflow.keras import layers, models
-from tensorflow.keras.regularizers import l2
+from tensorflow.keras import layers
 
 class AlexNet(tf.keras.Model):
-    def __init__(self, num_classes = 50):
+    def __init__(self, num_classes=10):  # Angepasst auf 10 Klassen
         super(AlexNet, self).__init__()
 
-        self.conv1 = layers.Conv2D(32, kernel_size = 7, strides = 4, padding = "same", kernel_regularizer = l2(0.0005))
+        # Erste Convolutional-Schicht
+        self.conv1 = layers.Conv2D(4, kernel_size=3, strides=1, padding="same")
         self.bn1 = layers.BatchNormalization()
         self.dropout_conv1 = layers.Dropout(0.3)
-        self.pool1 = layers.MaxPooling2D(pool_size = 3, strides = 2)
+        self.pool1 = layers.MaxPooling2D(pool_size=2, strides=2)
 
-        self.conv2 = layers.Conv2D(64, kernel_size = 5, strides = 1, padding = "same", kernel_regularizer = l2(0.0005))
+        # Zweite Convolutional-Schicht
+        self.conv2 = layers.Conv2D(8, kernel_size=3, strides=1, padding="same")
         self.bn2 = layers.BatchNormalization()
         self.dropout_conv2 = layers.Dropout(0.3)
-        self.pool2 = layers.MaxPooling2D(pool_size = 3, strides = 2)
+        self.pool2 = layers.MaxPooling2D(pool_size=2, strides=2)
 
-        self.conv3 = layers.Conv2D(96, kernel_size = 3, strides = 1, padding = "same", kernel_regularizer = l2(0.0005))
+        # Dritte Convolutional-Schicht
+        self.conv3 = layers.Conv2D(16, kernel_size=3, strides=1, padding="same")
         self.bn3 = layers.BatchNormalization()
         self.dropout_conv3 = layers.Dropout(0.4)
-        self.conv4 = layers.Conv2D(96, kernel_size = 3, strides = 1, padding = "same", kernel_regularizer = l2(0.0005))
+
+        self.conv4 = layers.Conv2D(16, kernel_size = 3, strides = 1, padding = "same")
         self.bn4 = layers.BatchNormalization()
         self.dropout_conv4 = layers.Dropout(0.4)
-        self.conv5 = layers.Conv2D(64, kernel_size = 3, strides = 1, padding = "same", kernel_regularizer = l2(0.0005))
+
+        # Fünfte Convolutional-Schicht
+        self.conv5 = layers.Conv2D(8, kernel_size=3, strides=1, padding="same")
         self.bn5 = layers.BatchNormalization()
         self.dropout_conv5 = layers.Dropout(0.4)
-        self.pool5 = layers.MaxPooling2D(pool_size = 3, strides = 2)
+        self.pool5 = layers.MaxPooling2D(pool_size=2, strides=2)
 
+        # Fully-Connected-Schichten
         self.flatten = layers.Flatten()
-        self.fc1 = layers.Dense(256, activation = "relu")
-        self.dropout1 = layers.Dropout(0.6)
-        self.fc2 = layer.Dense(128, activation = "relu")
-        self.dropout2 = layers.Dropout(0.6)
-        self.fc3 = layers.Dense(num_classes, activation = "softmax")
+        self.fc1 = layers.Dense(64, activation="relu")
+        self.dropout1 = layers.Dropout(0.5)
+        self.fc2 = layers.Dense(32, activation="relu")
+        self.dropout2 = layers.Dropout(0.5)
+        self.fc3 = layers.Dense(num_classes, activation="softmax")  # Output-Schicht für 10 Klassen
     
     def call(self, x):
+        # Forward Pass durch die Schichten
         x = self.conv1(x)
         x = self.bn1(x)
         x = tf.nn.relu(x)
@@ -51,10 +58,12 @@ class AlexNet(tf.keras.Model):
         x = self.bn3(x)
         x = tf.nn.relu(x)
         x = self.dropout_conv3(x)
+
         x = self.conv4(x)
         x = self.bn4(x)
         x = tf.nn.relu(x)
         x = self.dropout_conv4(x)
+
         x = self.conv5(x)
         x = self.bn5(x)
         x = tf.nn.relu(x)
